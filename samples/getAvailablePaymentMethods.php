049d536c520cb31c2d26d47f7740aa7a43dabff6
Dybo <Dybo@users.noreply.github.com>
2016-11-23 14:54:49 +0100

    add example GetAvailablePaymentMethods

--- samples/getAvailablePaymentMethods.php
+++ samples/getAvailablePaymentMethods.php
@@ -3,6 +3,10 @@ require_once '../vendor/autoload.php';
 require_once 'config.php';
 
 try {
+    
+    $methods = Service::getAvailablePaymentMethods([
+            'serviceId' => 'YOUR-SERVICE-ID',
+    ]);
 
 } catch (Exception $e) {
     echo "Error occurred: " . $e->getMessage();
