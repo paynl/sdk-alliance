4b21f5bf1ae89cc3b89a6c55b9ea83084ed1d272
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-12-22 12:50:19 +0100

    PLUG-1602 - Add merchant::addClearing function

---
+++ samples/addClearing.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../vendor/autoload.php';
+
+require_once 'config.php';
+
+try {
+    if (\Paynl\Alliance\Merchant::addClearing(
+        array(
+            'amount' => '123', /*(Required) The amount to clear, in cents.*/
+            'merchantId' => 'M-1234-5678', /*(Optional) The id of the merchant*/
+            'contentCategoryId' => '1234-5678' /*(Optional) The content category Id.*/
+        )
+    ) === true) {
+        echo 'Amount has been Cleared.';
+    }
+} catch (Exception $e) {
+    echo "Error occurred: " . $e->getMessage();
+}
